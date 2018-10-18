6dcfc33647d9b62df9b435f242146463ca99c508
Maksym Pavlushchenko <max.pavluschenko@gmail.com>
2018-10-18 15:47:48 +0300

    create pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'; 
 
 import { AppComponent } from './app.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductsEmployeesComponent } from './employees/products-employees.component';
+import { CustomersEmployeeComponent } from './employees/customers-employee.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { CustomersPageComponent } from './customers-page/customers-page.component';
+
+const appRoutes: Routes = [
+  { path: 'products', component: ProductsEmployeesComponent },
+  { path: 'customers', component: CustomersEmployeeComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsEmployeesComponent,
+    CustomersEmployeeComponent,
+    ProductsPageComponent,
+    CustomersPageComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

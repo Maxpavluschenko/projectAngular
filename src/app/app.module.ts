e7cfb5af98eeb7ba9afe280124317c5a6b34f91e
Maksym Pavlushchenko <max.pavluschenko@gmail.com>
2018-10-18 19:06:53 +0300

    create home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,28 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ProductsEmployeesComponent } from './employees/products-employees.component';
-import { CustomersEmployeeComponent } from './employees/customers-employee.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { ProductsPageComponent } from './products-page/products-page.component';
 import { CustomersPageComponent } from './customers-page/customers-page.component';
 
-const appRoutes: Routes = [
-  { path: 'products', component: ProductsEmployeesComponent },
-  { path: 'customers', component: CustomersEmployeeComponent },
-  { path: '', redirectTo: '/products', pathMatch: 'full' }
+const routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'products', component: ProductsPageComponent },
+  { path: 'customers', component: CustomersPageComponent }
+  // { path: '', redirectTo: '/products', pathMatch: 'full' }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsEmployeesComponent,
-    CustomersEmployeeComponent,
+    HomePageComponent,
     ProductsPageComponent,
     CustomersPageComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
